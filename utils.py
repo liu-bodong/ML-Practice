1172657594a6b32d1643bb8ce72f7cf5e8922c27
liubodong <liubodong04@gmail.com>
2025-06-28 22:21:14 +0800

    Implement convolution

--- utils.py
+++ utils.py
@@ -67,4 +67,10 @@ def init_weights(m):
         nn.init.xavier_uniform_(m.weight)
         nn.init.zeros_(m.bias)
 
-
+def corr2d(X, K):
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i:i+h, j:j+w] * K).sum()
+    return Y
